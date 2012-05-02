95487764bfe1effa33ac67b0d49587406837beeb
Wagner de Melo Reck <wagner.reck@gmail.com>
2012-05-02 20:29:53 -0300

    Added simple application to show debugger

--- exemploCmake/arquivo.c
+++ exemploCmake/arquivo.c
@@ -1,6 +1,12 @@
 #include "arquivo.h"
 
+
+
 int soma(int p1, int p2){
-	return p1 + p2;
+    
+    struct carta c1;
+    c1.n = COPAS;
+    
+	return p1 + p2 + 1;
 }
 
--- exemploCmake/main.c
+++ exemploCmake/main.c
@@ -2,6 +2,8 @@
 #include "arquivo.h"
 
 int main (int c, char**v){
-	printf ("%d\n",soma(2,1));
+    struct carta c1;
+    c1.n = COPAS;
+	printf ("%d\n",c1.n);
 	return 0;
 }
