ff569dd4038a56fd7423ea470a3a3598ad2ecb88
Fabiano Cassol <fabiano.babao@gmail.com>
2012-05-23 21:35:14 -0300

    Funcoes internas modificadas para static [2]

--- Baralho-Linguagem-C/corta.c
+++ Baralho-Linguagem-C/corta.c
@@ -37,7 +37,7 @@ Baralho cortar(Baralho B, int pos){
         return cortado;
 }
 
-static void insere_aux(Baralho *A, int carta, char naipe){
+void insere_aux(Baralho *A, int carta, char naipe){
 
         lista* novo = (lista*) malloc(sizeof(lista));
 
--- Baralho-Linguagem-C/corta.h
+++ Baralho-Linguagem-C/corta.h
@@ -6,34 +6,34 @@
  *
  *  Copyright 2012  Evair Severo <evairsevero@gmail.com>
  *
- *    
+ *
  *
  *
  *  This library is free software; you can redistribute it and/or
- *	
+ *
  *  modify it under the terms of the GNU Lesser General Public
  *
  *  License as published by the Free Software Foundation; either
- *	
+ *
  *  version 2.1 of the License, or (at your option) any later version.
- *	
- *	
+ *
+ *
  *  This library is distributed in the hope that it will be useful,
- *	
+ *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	
+ *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *	
+ *
  *  Lesser General Public License for more details.
- *	
- *	
+ *
+ *
  *  You should have received a copy of the GNU Lesser General Public
- *	
+ *
  *  License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- *	
+ *
  */
- 
- 
+
+
 /*!Corta um baralho em uma determinada posição.
  *@param B - Baralho a ser cortado.
  *@param pos - posição em que o baralho vai ser cortado.
@@ -47,4 +47,4 @@ Baralho cortar (Baralho B, int pos);
  *@param carta - Numero da carta que vai ser inserida.
  *@param carta - Naipe da carta.
  */
-static void insere_aux(Baralho* A, int carta, char naipe);
+void insere_aux(Baralho* A, int carta, char naipe);
