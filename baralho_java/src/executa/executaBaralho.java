4faa81158e7d2b319a10b9c00aa756ec29318fb9
root <root@nasser-3450.(none)>
2012-08-23 22:13:40 -0300

    Mandando o projeto

---
+++ baralho_java/src/executa/executaBaralho.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package executa;
+import Baralho;
+/**
+ *
+ * @author nasser
+ */
+public class executaBaralho {
+    public static void main(String args[]){
+        //Baralho b = new Baralho();
+        Baralho b = 
+    }
+    
+}
