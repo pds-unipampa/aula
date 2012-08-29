e5ccd0ca00c816638ba5942608b3c292fa06ef9c
Rafael Fernando dos Santos <rfs-rafael@hotmail.com>
2012-08-29 17:52:31 -0300

    Alterações Rafael Fernando dos Santos 111450026

--- baralho_java/src/Baralho.java
+++ baralho_java/src/Baralho.java
@@ -13,12 +13,11 @@ import javax.swing.JOptionPane;
 /**
  * Instancia um baralho completo com 52 cartas
  * @author nasser
+ * @author Rafael Fernando dos Santos 111450026
  */
 public class Baralho {
 
     static ArrayList<Carta> cartas = new ArrayList();
-    ArrayList<Naipe> naipes = new ArrayList();
-    ArrayList<Valor> valores = new ArrayList();
     static ArrayList<Carta> descarte = new ArrayList();
     private Carta descartando[];
 
@@ -27,40 +26,30 @@ public class Baralho {
      */
     public Baralho() {
 
-        naipes.add(Naipe.COPAS);
-        naipes.add(Naipe.ESPADA);
-        naipes.add(Naipe.OURO);
-        naipes.add(Naipe.PAUS);
-
-        valores.add(Valor.AS);
-        valores.add(Valor.DOIS);
-        valores.add(Valor.TRES);
-        valores.add(Valor.QUATRO);
-        valores.add(Valor.CINCO);
-        valores.add(Valor.SEIS);
-        valores.add(Valor.SETE);
-        valores.add(Valor.OITO);
-        valores.add(Valor.NOVE);
-        valores.add(Valor.DEZ);
-        valores.add(Valor.VALETE);
-        valores.add(Valor.DAMA);
-        valores.add(Valor.REI);
-
         preencheBaralho();
-
     }
 
     /**
      * Preenche o baralho com todas as cartas
      */
     private void preencheBaralho() {
-        for (int i = 0; i < valores.size(); i++) {
+        
+        for(Valor valor: Valor.values()){
+            
+            for(Naipe naipe: Naipe.values()){
+                
+                cartas.add(new Carta(valor, naipe));
+            }
+        }
+        
+        /*for (int i = 0; i < valores.size(); i++) {
             for (int j = 0; j < naipes.size(); j++) {
                 cartas.add(new Carta(valores.get(i), naipes.get(j)));
             }
 
-        }
+        }*/
     }
+    
     /**
      * Metodo que muda aleatoriamente a ordem das carta do baralho. Mas antes faz
     * a verificacao se existe alguma carta no baralho
@@ -71,6 +60,7 @@ public class Baralho {
             java.util.Collections.shuffle(cartas);
         }
     }
+    
     /**
      * Realiza o corte no baralho conforme entrada
      * aceita valores entre 2 e 50
@@ -99,6 +89,7 @@ public class Baralho {
             cartas.addAll(mCima);
         }
     }
+    
     /**
      * Coloca a primeira carta na parte de baixo do baralho
      */
@@ -114,49 +105,56 @@ public class Baralho {
      * @return carta
      */
     public Carta daUmaCartaInicio() {
-            Carta c = cartas.get(0);
-            cartas.remove(0);
-            return c;
-        }
+        
+        Carta c = cartas.get(0);
+        cartas.remove(0);
+        return c;
+    }
     
     /**
      * Distribui uma carta do Final do baralho
      * @return carta
      */
     public Carta daUmaCartaFinal() {
-            Carta c = cartas.get(cartas.size() - 1);
-            cartas.remove(cartas.size() - 1);
-            return c;
-        }
+        
+        Carta c = cartas.get(cartas.size() - 1);
+        cartas.remove(cartas.size() - 1);
+        return c;
+    }
     
     /**
      * Coloca a ultima carta na parte de cima do baralho
      */ 
     public void umaParaCima() {
-            ArrayList mCima = new ArrayList();
+        
+        ArrayList mCima = new ArrayList();
             
-            Carta c = cartas.get(cartas.size()-1);
-            cartas.remove(cartas.size()-1);
-            mCima.addAll(cartas);
-            cartas.clear();
-            cartas.add(c);
-            cartas.addAll(mCima);
-        }
+        Carta c = cartas.get(cartas.size()-1);
+        cartas.remove(cartas.size()-1);
+        mCima.addAll(cartas);
+        cartas.clear();
+        cartas.add(c);
+        cartas.addAll(mCima);
+    }
 
     /**
      * Adiciona uma carta ao monte de descarte
      * @param c Carta
      */
     public void colocaDescarte(Carta c){
+        
         descarte.add(c);
     }
+    
     /**
      * Retorna o tamanho do monte de descarte 
      * @return int tamanho de monte de descarte
      */
     public int tamanhoDescarte(){
+        
         return descarte.size();
     }
+    
     /**
      * Remove uma carta do descarte e retorna essa carta conforme a 
      * posição solicitada
@@ -164,6 +162,7 @@ public class Baralho {
      * @return carta
      */
     public Carta removeCartaDescarte(int posicao){
+        
         return descarte.remove(posicao);
     }
     
@@ -173,22 +172,24 @@ public class Baralho {
      * @return carta
      */
     public Carta visualizaCartaDescarte(int posicao){
+        
         return descarte.get(posicao);
     }
-    
+   
     /*
      * Método que retorna o monte de descarte
      * @return array com cartas descartadas no jogo
      */
     public Carta[] PegaDescarteInteiro(){
        
-       for (int i = 0; i <= tamanhoDescarte(); i++) {
+       /*for (int i = 0; i <= tamanhoDescarte(); i++) {
            descartando[i] =  descarte.remove(i);
-       }
- 
-            return descartando;
+       }*/
+        descartando = (Carta[]) descarte.toArray();
+        descarte.clear();
+            
+        return descartando;
     }
-    
     
     /**
      * Metodo de uso interno que faz a verificacao do baralho passado por parametro
@@ -212,35 +213,51 @@ public class Baralho {
      */
     
     public int verificaPar(Carta c){
+        
         int par = 0;
+       
         switch (c.getValor().toString()){
                 case "AS":
                         par = 1;
+                    break;
                 case "DOIS":
                         par = 0;
+                    break;
                 case "TRES":
                         par = 1;    
+                    break;
                 case "QUATRO":
                         par = 0;    
+                    break;
                 case "CINCO":
                         par = 1;    
+                    break;
                 case "SEIS":
                         par = 0;    
+                    break;
                 case "SETE":
                         par = 1;    
+                    break;
                 case "OITO":
                         par = 0;    
+                    break;
                 case "NOVE":
                         par = 1;    
+                    break;
                 case "DEZ":
                         par = 0;    
+                    break;
                 case "VALETE":
                         par = 1;    
+                    break;
                 case "DAMA":
                         par = 0;    
+                    break;
                 case "REI":
                         par = 1;    
+                    break;
         }    
+        
         return par;
     }
 
