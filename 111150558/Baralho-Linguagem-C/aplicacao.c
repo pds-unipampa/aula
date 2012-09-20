a977eb91bfd69d9e4fa52218f63eb8e4880f8da6
Cristiano Ribeiro <cristiano.daitx@gmail.com>
2012-09-20 17:11:18 -0300

    jogo funcionando para até 32 jogadores

--- 111150558/Baralho-Linguagem-C/aplicacao.c
+++ 111150558/Baralho-Linguagem-C/aplicacao.c
@@ -46,6 +46,7 @@ int main(){
     int i, aux;
     int numjogador;
     char escolha[10];
+    char fim[4] = "fim";
     Baralho B = cria();
     Baralho D = cria();
     Nome *nomes; 
@@ -54,7 +55,8 @@ int main(){
 	printf("\nInsira a quantidade de jogadores:");
 	scanf("%d",&numjogador);
 	getchar();
-	nomes=crianomes(numjogador);
+	nomes=crianomes(32);
+	aux =0;
 	aux = numjogador;
     for (i=0;i<numjogador;i++){
 		printf("\nInsira o nome do Jogador %d : ",i);
@@ -64,45 +66,54 @@ int main(){
     srand(time(NULL));
     B = embaralhar(B);
 
-    while (aux<=1){
+    while (aux>1){
 		for (i=0;i<numjogador;i++){
 				printf("\nCarta: %d de %s\n", B->carta, strings[B->naipe]);
-				C = retiraInicio(&B);
-				D = cria();
-				D = insereCartaDescarte(D, C.carta, C.naipe);
-				if ( (strcmp(nomes[i].nome,"fim")) == 0){
+				if ( (strcmp(nomes[i].nome,"fim")) != 0){
 					printf("\n\n %s. maior ou menor?\n", nomes[i].nome);
 					gets(escolha);     
-					if  ((((!(strcmp(escolha,"maior"))) == 0) && ((B->carta)>(D->carta))) || ((!(strcmp(escolha,"menor")) == 0) && ((B->carta)<(D->carta)))) {
-
-						printf("\nBoa escolha!!");
-
-					}
-					else{
-
+					C = retiraInicio(&B);
+					D = cria();
+					D = insereCartaDescarte(D, C.carta, C.naipe);
+					if  ((strcmp(escolha,"maior")) == 0) { 
+						if ((D->carta)<=(B->carta)){
+							printf("\nCarta: %d de %s\n", B->carta, strings[B->naipe]);
+							printf("\nBoa escolha!!");
+						}
+						else{
+						printf("\nCarta: %d de %s\n", B->carta, strings[B->naipe]);
 						printf("\nFim de jogo para %s\n",nomes[i].nome);
 						nomes[i].nome="fim";
-						
+						aux = aux--;
+					}
+					}
+					if ((strcmp(escolha,"menor")) == 0){
+						if ((D->carta)>=(B->carta)){
+							printf("\nCarta: %d de %s\n", B->carta, strings[B->naipe]);
+							printf("\nBoa escolha!!");
+						}
+						else{
+						printf("\nCarta: %d de %s\n", B->carta, strings[B->naipe]);
+						printf("\nFim de jogo para %s\n",nomes[i].nome);
+						nomes[i].nome="fim";
+						aux = aux--;
+					}
 					}
 				}
+				}
 		}		
-		aux = aux--;
-	}
-
-   
+		
     printf("\n\nFIM DE JOGO!\n");
     for (i=0;i<numjogador;i++){
 		if ( (strcmp(nomes[i].nome,"fim")) != 0){
 				printf("\n\n O(a) vencedor(a) foi o(a) %s. \n", nomes[i].nome);
 			}
-        return 0;
     }
 
 
     liberaBaralho(&B);
     liberaBaralho(&D);
 
-	printf("\n nome1 - %s \n nome2 - %s\n",nomes[0].nome, nomes[1].nome);
     return 0;
 }
 
