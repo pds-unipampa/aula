6d1e387ae87ec3c8a77a5878e250e06283aff46f
= <=>
2012-10-09 19:19:44 -0300

    instalador feito

---
+++ 111151967/adivinha/build/CMakeFiles/TestBaralho.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/ezequiel/Área de Trabalho/repositorio/aula/111151967/adivinha/Baralho-Linguagem-C/Testes/minunit.c" "/home/ezequiel/Área de Trabalho/repositorio/aula/111151967/adivinha/Baralho-Linguagem-C/build/CMakeFiles/TestBaralho.dir/Testes/minunit.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ezequiel/Área de Trabalho/repositorio/aula/111151967/adivinha/Baralho-Linguagem-C/build/CMakeFiles/Baralho.dir/DependInfo.cmake"
+  )
---
+++ 111151967/adivinha/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ezequiel/Área de Trabalho/repositorio/aula/111151967/adivinha/Baralho-Linguagem-C
+# Build directory: /home/ezequiel/Área de Trabalho/repositorio/aula/111151967/adivinha/Baralho-Linguagem-C/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(TestBaralho "TestBaralho")
