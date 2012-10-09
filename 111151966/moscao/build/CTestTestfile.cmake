f28297425207b67e297e46ef6193b42ba380c495
Evair <evair@evair-RF511-RF411-RF711.(none)>
2012-10-09 18:23:15 -0300

    Build apagado

--- 111151966/moscao/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/evair/Documentos/moscao
-# Build directory: /home/evair/Documentos/moscao/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(TestBaralho "TestBaralho")
